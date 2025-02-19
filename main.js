f39fea5125d9ba13ec168330f650b0108715db9d
Oluwabukunmi Victoria Adebayo <adebayooluwabukunmi14@gmail.com>
2025-02-19 09:04:00 +0100

    hello

---
+++ main.js
@@ -0,0 +1,23 @@
+let cart = 0
+  
+  const carts = document.querySelector('.fa-solid')
+  const button = document.querySelectorAll('.submit').forEach(button =>{
+    
+    carts.innerHTML = cart
+    button.addEventListener('click',() =>{
+      carts.innerHTML = cart++
+  })
+  
+  })
+  const productImage = document.querySelector('.prdt-ctn');
+const addToCartButton = document.querySelector('.submit');
+
+const cartItems = document.querySelector('#cart-items');
+
+addToCartButton.addEventListener('click', () => {
+  const clonedImage = productImage.cloneNode(true);
+  clonedImage.classList.add('cart-item-image');
+  
+  // Append the cloned image to the cart items container
+  cartItems.appendChild(clonedImage);
+});
