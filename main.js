979ec23677db0086cd2faba78e32ff33a64ca26e
Oluwabukunmi Victoria Adebayo <adebayooluwabukunmi14@gmail.com>
2025-02-20 14:50:11 +0100

    latest

--- main.js
+++ main.js
@@ -10,15 +10,3 @@ let cart = 1
   })
   
   })
-  const productImage = document.querySelector('.prdt-ctn');
-const addToCartButton = document.querySelector('.submit');
-
-const cartItems = document.querySelector('#cart-items');
-
-addToCartButton.addEventListener('click', () => {
-  const clonedImage = productImage.cloneNode(true);
-  clonedImage.classList.add('cart-item-image');
-  
-  // Append the cloned image to the cart items container
-  cartItems.appendChild(clonedImage);
-});
